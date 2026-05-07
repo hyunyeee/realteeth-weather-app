852fdaecd724e48214bcdc0b04dd8d2a5e293a03
hyunyeee <5004ohy@naver.com>
2026-05-07 17:11:14 +0900

    chore: Next.js, Tailwind, FSD 구조 초기 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { pretendard } from "@/styles/font";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "리얼티쓰 날씨 앱 - 오현의  ",
+  description: "프론트엔드 개발자 채용 날씨 앱 구현 과제 - 오현의 ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko" className={`${pretendard.variable} h-full min-h-screen`}>
+      <body
+        className={`${pretendard.className} h-full min-h-screen w-full overflow-auto text-black`}
+      >
+        <main className="flex-1">{children}</main>
+      </body>
+    </html>
+  );
+}
